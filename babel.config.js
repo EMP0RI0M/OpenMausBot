42e8ecd8a19a607634240bbe309a30a02d5bdc42
root <root@localhost.localdomain>
2026-09-20 04:38:06 -1000

    fix(mobile): add @babel/plugin-transform-classes for Hermes class inheritance compatibility

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,7 @@ module.exports = function(api) {
       ['babel-preset-expo', { jsxImportSource: 'react' }]
     ],
     plugins: [
+      '@babel/plugin-transform-classes',
       '@babel/plugin-transform-class-properties',
       '@babel/plugin-transform-private-methods',
       '@babel/plugin-transform-private-property-in-object',
