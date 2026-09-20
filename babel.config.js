22a172954bd7fd32bd7076468fc3d6e7bbb7918a
root <root@localhost.localdomain>
2026-09-20 04:20:59 -1000

    fix(mobile): add babel private properties plugins for Hermes bytecode support

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'react' }]
+    ],
+    plugins: [
+      '@babel/plugin-transform-class-properties',
+      '@babel/plugin-transform-private-methods',
+      '@babel/plugin-transform-private-property-in-object',
+    ],
   };
 };
